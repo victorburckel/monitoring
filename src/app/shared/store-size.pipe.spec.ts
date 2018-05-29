f3e162091b2da0443c98fc1c34d2b0ff596e684c
victorburckel <victor.burckel@gmail.com>
2018-05-29 21:53:23 +0200

    Remove non developed test files

--- src/app/shared/store-size.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StoreSizePipe } from './store-size.pipe';
-
-describe('SoreSizePipe', () => {
-  it('create an instance', () => {
-    const pipe = new StoreSizePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
